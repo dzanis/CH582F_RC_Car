c1ea4711942b780cf50d6eabd952ec1c57ad202c
dzanis <zanisdukalskis@gmail.com>
2025-10-30 23:37:17 +0200

    add rc car web ble

---
+++ rc_car_web_ble/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "ble-car-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./joy.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
+  );
+});
